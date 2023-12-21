76442bd6d0ede00005283e23382155579ff3a554
Devin Otto <devindotto@gmail.com>
2023-12-20 18:36:33 -1000

    main menu work and stuff

---
+++ Assets/Scripts/AlternatingColors.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class AlternatingColors : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    public float revealDelay = 2.0f; // Time delay between line reveals
+    public bool startIntro = false; // Set this to true in the Inspector or elsewhere to start the intro
+
+    void Start()
+    {
+        // Check if startIntro is true before starting the coroutine
+        if (startIntro && isActiveAndEnabled)
+        {
+            StartCoroutine(RevealText());
+        }
+    }
+
+    IEnumerator RevealText()
+    {
+        string[] lines = {
+            "In a candy forest where Christmas was lost,",
+            "Santa's sleigh broken, Rudolph's fate crossed.",
+            "Tethers to mortality severed and torn,",
+            "Eldritch Rudolph, in shadows, is born.",
+            "Four sleigh parts scattered, survival at stake,",
+            "Through sweet illusions, the terror awaits.",
+            "In peppermint glades, where nightmares deploy,",
+            "Escape the eldritch, recover the joy."
+        };
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            // Concatenate lines up to the current index
+            string visibleText = string.Join("\n", lines, 0, i + 1);
+            textMeshPro.text = visibleText;
+
+            // Wait for the specified reveal delay
+            yield return new WaitForSeconds(revealDelay);
+        }
+    }
+
+    // Public method to set startIntro to true from other scripts
+    public void StartIntroFromOtherScript()
+    {
+        startIntro = true;
+        StartCoroutine(RevealText());
+    }
+}
---
+++ Assets/Scripts/Scene Management.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Ensure only one instance of the SceneLoader exists
+    private static SceneManagement instance;
+
+    public GameObject objectToActivate; // Reference to the GameObject to activate
+    public AlternatingColors alternatingColorsScript; // Reference to the AlternatingColors script
+
+    private void Awake()
+    {
+        // Check if an instance already exists
+        if (instance == null)
+        {
+            // If not, set the instance to this and mark it as not to be destroyed
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // If an instance already exists, destroy this GameObject
+            Destroy(gameObject);
+        }
+    }
+
+    // Attach this method to a UI button click event
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        // Call a function to activate a GameObject (you can adjust this condition as needed)
+        if (sceneName == "YourTargetSceneName")
+        {
+            StartIntro();
+        }
+    }
+
+    // Function to activate the specified GameObject and set startIntro to true
+    public void StartIntro()
+    {
+        if (alternatingColorsScript != null)
+        {
+            alternatingColorsScript.startIntro = true;
+        }
+
+        if (objectToActivate != null)
+        {
+            objectToActivate.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("objectToActivate is not assigned in the inspector.");
+        }
+    }
+}
